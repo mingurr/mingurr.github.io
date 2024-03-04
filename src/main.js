7a51645068c8f3b27cfe9bc0b46782b2b5294c55
mingurr <alsrud9138@gmail.com>
2024-03-04 22:31:07 +0900

    JS - Home, Arrow

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// 메뉴 배경 색 바꾸기
 const header = document.querySelector('.header');
 const headerRect = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -6,4 +7,22 @@ document.addEventListener('scroll', () => {
     } else {
         header.classList.remove('header--dark')}
 })
+
+// 홈화면 투명하게 만들기
+const homeContainer = document.querySelector('.home__container');
+const homeRect = home.offsetHeight;
+document.addEventListener('scroll', () => {
+    homeContainer.style.opacity = 1 - window.scrollY / homeRect;
+})
+
+// 화살표 버튼 동작
+const arrow = document.querySelector('.arrow-up');
+document.addEventListener('scroll', () => {
+    if (window.scrollY > homeRect/2) {
+        arrow.classList.add('arrow--appear')
+    } else {
+        arrow.classList.remove('arrow--appear')}
+})
+
+//토글버튼
 
